d7f23a47618a747d3872cae4b1a53443f0a57895
pyed <iAbdulelah@Gmail.com>
2014-01-05 03:47:56 +0300

    import the right module

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from distutils.core import setup
 
 config = {
         'name': 'qBittorrentPy',
-        'version': 0.1,
+        'version': '0.1',
         'description': 'python module to interact with qBittorrent API',
         'author': 'Abdulelah Alfntokh',
         'author_email': 'iAbdulelah@gmail.com',
         'url': 'http://github.com/pyed/qbittorrentpy',
         'packages': ['qbittorrentpy'],
-        'install_requires': ['requests']
         }
 
 setup(**config)
